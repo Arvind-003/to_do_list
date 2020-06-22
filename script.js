049e3f06923f8586f7f628e0da563c4bda5ad5d2
Arvind Kumar Mahto <arvind.kumar8271043313@gmail.com>
2020-06-22 11:55:04 +0530

    responsive made

--- script.js
+++ script.js
@@ -5,38 +5,36 @@ var i = 0;
 var count = document.getElementsByClassName("countDiv")[0];
 count.innerText = i;
 addToDoButton.addEventListener("click", function (e) {
-  // create li Element
-
   var li = document.createElement("li");
-// class added
+
   li.className = "collection-item";
-//   created text node and appended to li
+
   li.appendChild(document.createTextNode(inputField.value));
-  
-  li.style.marginLeft = "30px";
-  
-  li.style.fontSize = "1.6rem";
-  
+
+  li.style.marginLeft = "5%";
+
+  li.style.fontSize = "inherit";
+
   li.style.color = "white";
-//   created anchor tag for cross
+
   const link = document.createElement("a");
-//   added class to anchor tag
+
   link.className = "delete-item secondary-content";
-//   added cross using font awesome
+
   link.innerHTML = '  <i class="fas fa-times"></i>';
-//   appended the link to  li
+
   li.appendChild(link);
-  
+
   const taskList = document.querySelector("#toDoContainer");
-//   appended li to ul
+
   taskList.appendChild(li);
-//   for clearing the input box
+
   inputField.value = "";
-//   now we are doing css part for the cross;
+
   var cross = document.getElementsByClassName("delete-item")[i];
   cross.style.cursor = "pointer";
   cross.style.float = "right";
-//   to keep track of index and number of task created;
+
   i++;
   var count = document.getElementsByClassName("countDiv")[0];
   count.innerText = i;
